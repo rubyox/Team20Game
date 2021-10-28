c0251453a5c8242a5dc76172d1b7122021af2955
rubyox <rubyox123@gmail.com>
2021-10-28 15:46:41 +0100

    Network Prototype
    Working network included build

--- Techno Duels/Assets/Scripts/mainMenu.cs
+++ Techno Duels/Assets/Scripts/mainMenu.cs
@@ -2,16 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Realtime;
+using Photon.Pun;
 
-public class mainMenu : MonoBehaviour
+public class mainMenu : MonoBehaviourPunCallbacks
 {
-    public void PlayGame()
+   
+    
+    void Start()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        PhotonNetwork.ConnectUsingSettings();
     }
-    public void QuitGame()
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Server");
+        PhotonNetwork.AutomaticallySyncScene=true;
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Could not find game, creating room");
+        MakeRoom();
+    }
+
+    void MakeRoom()
+    {
+        int randomRoomName = Random.Range(0, 5000);
+        RoomOptions roomOptions = new RoomOptions() {
+            IsVisible = true,
+            IsOpen = true,
+            MaxPlayers = 2
+        };
+        PhotonNetwork.CreateRoom("RoomName_" + randomRoomName, roomOptions);
+        Debug.Log("Room Created, Waiting for Another Player");
+    }
+
+    public void playGame()
+    {
+        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("Searching for a Game");
+        
+    }
+
+    public void stopSearch()
+    {
+        PhotonNetwork.LeaveRoom();
+        Debug.Log("Canceled Search");
+    }
+
+    public void quitGame()
     {
         Debug.Log("QUIT!");
         Application.Quit();
     }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 2 && PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log(PhotonNetwork.CurrentRoom.PlayerCount + "/2 Starting Game");
+            PhotonNetwork.LoadLevel(1);
+        }
+
+    }
+    
 }
