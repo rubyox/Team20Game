31d3a84e02bed18c0989388aa3407029f8ae9644
Callum <200057460@ASTON.AC.UK>
2021-11-06 01:36:12 +0000

    UI-Changes
    Added UI Cards and a bottom UI element, as well as a background

---
+++ Techno Duels/Assets/Unit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+  public string userName;
+
+  public int damage;
+  public int currentHP;
+  public int maxHP;
+
+}
